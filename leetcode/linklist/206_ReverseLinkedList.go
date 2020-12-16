56b8dfa3805277c1747b96fae39f97b290ec3f7a
luffych <luuucode@gmail.com>
2020-12-16 15:06:47 +0800

    reverse list & todo solve 空间复杂度

--- leetcode/linklist/206_ReverseLinkedList.go
+++ leetcode/linklist/206_ReverseLinkedList.go
@@ -15,7 +15,6 @@ A linked list can be reversed either iteratively or recursively. Could you imple
 链接：https://leetcode-cn.com/problems/reverse-linked-list
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -23,7 +22,23 @@ A linked list can be reversed either iteratively or recursively. Could you imple
  *     Next *ListNode
  * }
  */
-//TODO 待实现
-func reverseList(head *ListNode) *ListNode {
-	return head
+
+//当前版本 时间复杂度O(n) 空间复杂度O(n)
+func ReverseList1(head *ListNode) *ListNode {
+	res := &ListNode{head.Val,nil}
+	node := head.Next
+	for node != nil {
+		res = &ListNode{node.Val,res}
+		node = node.Next
+	}
+	return res
+}
+
+//todo
+func ReverseList2(head *ListNode) *ListNode{
+	var prev *ListNode = nil
+	for head.Next != nil {
+
+	}
+	return prev
 }
--- leetcode/linklist/leet_linklist_test.go
+++ leetcode/linklist/leet_linklist_test.go
@@ -13,3 +13,17 @@ func TestCount(t *testing.T) {
 	t.Log(Count(&head))
 	t.Log(IsPalindrome(&head))
 }
+
+func TestReverseList(t *testing.T) {
+	node4 := ListNode{5, nil}
+	node3 := ListNode{4, &node4}
+	node2 := ListNode{3, &node3}
+	node1 := ListNode{2, &node2}
+	head := ListNode{1, &node1}
+
+	reverse := ReverseList2(&head)
+	for reverse != nil {
+		t.Log(reverse.Val)
+		reverse = reverse.Next
+	}
+}
